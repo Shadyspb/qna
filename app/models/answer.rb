4ee437116ffa5b3243949c477f472feba80a1ae2
shady <Shad1k@myttk.ru>
2018-02-10 00:48:19 +0300

    Polymorphic attachments. Answers associated with attachments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,13 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
+  has_many :attachments, as: :attachmentable
 
   validates :body, presence: true
 
   scope :sort_by_best, -> {order(best: :desc)}
+
+  accepts_nested_attributes_for :attachments
 
   def mark_best
     transaction do
--- spec/acceptance/add_files_to_question_spec.rb
+++ spec/acceptance/add_files_to_question_spec.rb
@@ -13,12 +13,12 @@ background do
     visit new_question_path
   end
 
-  scenarion 'User adds file when asks question' do
+  scenario 'User adds file when asks question' do
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'text text'
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
     click_on 'Create'
 
-    expect(page).to have_content 'spec_helper.rb'
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
   end
 end
